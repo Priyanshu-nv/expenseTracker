63ea9492a054fb1ef1f7c5184d99d40fd1bee38a
priyanshu navidolatwala <133123493+Priyanshu-nv@users.noreply.github.com>
2025-04-18 08:31:16 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -27,7 +27,7 @@ function Signup() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = `${APIUrl}/auth/signup`;
+            const url = `https://expense-tracker-pink-beta-20.vercel.app/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
